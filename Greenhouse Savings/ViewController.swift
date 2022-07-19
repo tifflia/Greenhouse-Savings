93c65f209187e9dd82a053c863100fb340c8c39b
Tiffany <tiffanyjxliang@gmail.com>
2022-07-19 13:39:24 -0700

    Initial Commit

---
+++ Greenhouse Savings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greenhouse Savings
+//
+//  Created by Tiffany Liang on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Greenhouse SavingsUITests/Greenhouse_SavingsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Greenhouse_SavingsUITestsLaunchTests.swift
+//  Greenhouse SavingsUITests
+//
+//  Created by Tiffany Liang on 7/19/22.
+//
+
+import XCTest
+
+class Greenhouse_SavingsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
