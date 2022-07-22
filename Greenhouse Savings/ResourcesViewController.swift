da81bf3a81ebc83c3bb9a7239d1e615543e7a79b
SOPHIA AN <sotoky14@gmail.com>
2022-07-21 18:28:35 -0700

    added buttons + resources

--- Greenhouse Savings/ResourcesViewController.swift
+++ Greenhouse Savings/ResourcesViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 class ResourcesViewController: UIViewController {
 
     @IBOutlet weak var buttonResourceOne: UIButton!
+    @IBOutlet weak var buttonResorceTwo: UIButton!
+    @IBOutlet weak var buttonResourceThree: UIButton!
+    @IBOutlet weak var buttonResouceFour: UIButton!
+    @IBOutlet weak var buttonResourceFive: UIButton!
+    @IBOutlet weak var resourceButtonSix: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,7 +23,27 @@ class ResourcesViewController: UIViewController {
         let url = URL (string: "https://www.realsimple.com/work-life/money/saving/self-care-tips-budget")!
         UIApplication.shared.open(url)
     }
+    @IBAction func buttonResourceTwo(_ sender: Any) {
+        let url = URL (string:"https://www.capitalone.com/learn-grow/money-management/money-management-tips/")!
+        UIApplication.shared.open(url)
+    }
+    @IBAction func buttonResourceThree(_ sender: Any) {
+        let url = URL (string:"https://www.businessinsider.com/personal-finance/how-to-save-money-as-a-teenager")!
+        UIApplication.shared.open(url)
+    }
     
+    @IBAction func buttonResouceFour(_ sender: Any) {
+        let url = URL(string:"https://mint.intuit.com/blog/budgeting/budgeting-for-teens/")!
+        UIApplication.shared.open(url)
+    }
+    @IBAction func buttonResouceFive(_ sender: Any) {
+        let url = URL(string:"https://money.usnews.com/money/personal-finance/spending/articles/fast-fashion-how-to-avoid-it-even-on-a-budget")!
+        UIApplication.shared.open(url)
+    }
+    @IBAction func resouceButtonSix(_ sender: Any) {
+        let url = URL(string:"https://www.vogue.com/article/save-money-on-clothes-alexa-von-tobel")!
+        UIApplication.shared.open(url)
+    }
 }
     
 
